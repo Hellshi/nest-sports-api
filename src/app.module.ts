17e54ec84f4cd2dfc6a168210fc358c7152d02f5
Hellem <hcslimaa@gmail.com>
2022-03-05 13:46:45 -0300

    feat: added database connection and env configs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,26 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import config from './configuration/config';
 import { PlayersModule } from './players/players.module';
 
 @Module({
-  imports: [PlayersModule],
+  imports: [
+    PlayersModule,
+    ConfigModule.forRoot({
+      load: [],
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: config().database.host,
+      port: config().database.port,
+      username: config().database.user,
+      password: config().database.password,
+      database: config().database.database,
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
